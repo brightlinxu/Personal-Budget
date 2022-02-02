53c729c9a3441192efe930bc77a9cca756367fff
Bright Xu <brightxu@umich.edu>
2022-02-02 04:04:36 -0500

    edited history button

--- src/components/BudgetBoxes.vue
+++ src/components/BudgetBoxes.vue
@@ -4,27 +4,19 @@
       :id='i' 
     />
   </div>
-  <div @click='redirectHistory'>History</div>
 </template>
 
 <script>
 import { useStore } from 'vuex';
-import { useRouter } from 'vue-router';
 // import { computed } from 'vue';
 import BudgetBox from './BudgetBox.vue';
 
 export default {
   setup() {
     const store = useStore();
-    const router = useRouter();
-
-    const redirectHistory = () => {
-      router.push('history');
-    }
 
     return {
       store: store.state,
-      redirectHistory
     }
   },
   components: {
--- src/views/Budget.vue
+++ src/views/Budget.vue
@@ -1,20 +1,30 @@
 <template>
   <div v-if='store.authIsReady && store.dataIsReady'>
     <BudgetBoxes />
+    <div @click='redirectHistory' class='historyContainer'>
+      <div class='buttonStyle' style='width: 51px;'>History</div>
+    </div>
   </div>
 </template>
 
 <script>
 import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
 // import { computed } from 'vue';
 import BudgetBoxes from '../components/BudgetBoxes.vue';
 
 export default {
   setup() {
     const store = useStore();
+    const router = useRouter();
+
+    const redirectHistory = () => {
+      router.push('history');
+    }
 
     return { 
       store: store.state,
+      redirectHistory
     };
   },
   components: {
@@ -23,6 +33,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.historyContainer {
+  display: flex;
+  justify-content: center;
+  margin-top: 15px;
+  margin-bottom: 30px;
+}
 </style>
